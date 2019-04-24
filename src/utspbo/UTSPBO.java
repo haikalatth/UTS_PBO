740f60a0b1667b9b2177a773203b645f677aa586
micin <micin@root-pc>
2019-04-24 22:12:28 +0700

    main

--- src/utspbo/UTSPBO.java
+++ src/utspbo/UTSPBO.java
@@ -15,6 +15,14 @@ public class UTSPBO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {    
+        Intel Intel1 = new Intel(2);
+        Amd Amd1 = new Amd(3);
+        Komputer Komputer1 = new Komputer(Intel1);
+        
+        Komputer1.cetakInfo();        
+        Komputer1.mencabut(Amd1);
+        Komputer1.memasang(Intel1);
+        Komputer1.cetakInfo();
         // TODO code application logic here
     }
     
